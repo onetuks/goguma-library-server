f31ea2852927e5a84129e5091e5e1571ba5b15dc
onetuks <onetuks@naver.com>
2024-06-02 00:25:03 +0900

    Chore: Flyway DDL 작성
    #1

--- src/main/java/com/onetuks/gogumalibraryserver/GogumaLibraryServerApplication.java
+++ src/main/java/com/onetuks/gogumalibraryserver/GogumaLibraryServerApplication.java
@@ -2,8 +2,10 @@ package com.onetuks.gogumalibraryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.onetuks")
 public class GogumaLibraryServerApplication {
 
   public static void main(String[] args) {
