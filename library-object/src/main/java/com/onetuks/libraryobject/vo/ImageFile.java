ea30d16b0919a741dc37cd3ef9ce50421249e9ab
onetuks <onetuks@naver.com>
2024-06-24 17:02:30 +0900

    Feat: 기본 프로필 이미지 이름 static 설정

--- library-object/src/main/java/com/onetuks/libraryobject/vo/ImageFile.java
+++ library-object/src/main/java/com/onetuks/libraryobject/vo/ImageFile.java
@@ -5,12 +5,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 public record ImageFile(ImageType imageType, MultipartFile file, String fileName) {
 
+  private static final String DEFAULT_PROFILE_IMAGE_URI = "default_profile_image.png";
+
   public static ImageFile of(ImageType imageType, MultipartFile file, String fileName) {
     return new ImageFile(imageType, file, fileName);
   }
 
   public static ImageFile of(ImageType imageType, String uri) {
     return new ImageFile(imageType, null, uri);
+  }
+
+  public static String getDefaultProfileImagUri() {
+    return ImageType.PROFILE_IMAGE.getDirectoryPath() + DEFAULT_PROFILE_IMAGE_URI;
   }
 
   public String getUri() {
