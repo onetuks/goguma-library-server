dcb9a18990676823af1667e36db1e2c3b1a87926
onetuks <onetuks@naver.com>
2024-08-15 14:40:52 +0900

    Refactor: Cache 키 static 변수 처리

--- library-object/src/main/java/com/onetuks/libraryobject/enums/CacheName.java
+++ library-object/src/main/java/com/onetuks/libraryobject/enums/CacheName.java
@@ -1,10 +1,15 @@
 package com.onetuks.libraryobject.enums;
 
 public interface CacheName {
+  // Cache Name
   String WEEKLY_FEATURED_BOOKS = "weekly_featured_books";
   String RECOMMENDED_MEMBERS = "recommended_members";
   String REVIEW_FEED = "review_feed";
   String BOOK_PICKS = "book_picks";
   String REVIEW_PICKS = "review_picks";
   String MEMBER_FOLLOWS = "member_follows";
+
+  // Cache Key
+  String FEATURED_BOOKS_CACHE_KEY = "'featured_books'";
+  String RECOMMENDED_MEMBERS_CACHE_KEY = "'recommendedMembers'";
 }
