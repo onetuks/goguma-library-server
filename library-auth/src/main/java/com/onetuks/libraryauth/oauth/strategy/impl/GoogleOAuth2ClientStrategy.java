9a8cd766515dfb010909492fc18cfcc564628b5b
onetuks <onetuks@naver.com>
2024-08-16 15:24:32 +0900

    Refactor: OAuth2 로그인 실패 로그 추가

--- library-auth/src/main/java/com/onetuks/libraryauth/oauth/strategy/impl/GoogleOAuth2ClientStrategy.java
+++ library-auth/src/main/java/com/onetuks/libraryauth/oauth/strategy/impl/GoogleOAuth2ClientStrategy.java
@@ -60,7 +60,7 @@ public class GoogleOAuth2ClientStrategy implements OAuth2ClientStrategy {
                         .bodyToMono(String.class)
                         .flatMap(
                             errorBody -> {
-                              log.warn("구글 토큰 요청 실패 - errorBody: {}", errorBody);
+                              log.warn("구글 유저정보 요청 실패 - errorBody: {}", errorBody);
                               return Mono.error(
                                   new TokenValidFailedException(ErrorCode.UNAUTHORIZED_TOKEN));
                             }))
@@ -100,7 +100,14 @@ public class GoogleOAuth2ClientStrategy implements OAuth2ClientStrategy {
         .onStatus(
             HttpStatusCode::is4xxClientError,
             clientResponse ->
-                Mono.error(new TokenValidFailedException(ErrorCode.UNAUTHORIZED_TOKEN)))
+                clientResponse
+                    .bodyToMono(String.class)
+                    .flatMap(
+                        errorBody -> {
+                          log.warn("구글 토큰 요청 실패 - errorBody: {}", errorBody);
+                          return Mono.error(
+                              new TokenValidFailedException(ErrorCode.UNAUTHORIZED_TOKEN));
+                        }))
         .onStatus(
             HttpStatusCode::is5xxServerError,
             clientResponse ->
