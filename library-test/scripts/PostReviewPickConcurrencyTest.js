e1561a4c699f13b7d7ffc4eba1b3f71b7e834e5c
onetuks <onetuks@naver.com>
2024-08-29 16:21:53 +0900

    Feat: 서평픽 등록 동시성 테스트 스크립트 추가

--- library-test/scripts/PostReviewPickConcurrencyTest.js
+++ library-test/scripts/PostReviewPickConcurrencyTest.js
@@ -1,11 +1,32 @@
 import http from "k6/http";
+import {check} from "k6";
 
 export let options = {
-  vus: 1,
-  duration: '30s',
+  vus: 100,
+  iterations: 100,
 };
 
+// 토큰 발급을 위한 함수
+function getToken() {
+  const authUrl = 'http://localhost:8080/api/test/login';
+  const authRes = http.post(authUrl);
+
+  return JSON.parse(authRes.body).appToken;
+}
+
 export default function() {
-  let getUrl = "http://localhost:8080/reviews/1/picks"
-  http.post(getUrl);
+  const reviewId = 1;
+
+  const url = `http://localhost:8080/api/reviews/${reviewId}/picks`;
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${getToken()}`,
+    }
+  }
+
+  const res = http.post(url, null, params);
+  check(res, {
+    "status is 201": (r) => r.status === 201
+  });
 }
